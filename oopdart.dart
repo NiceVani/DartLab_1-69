3213fc40fda0754c0e78e3fa78192d21daaad637
NiceVani <Nice090147022165@gmail.com>
2026-07-13 22:29:59 +0700

    ll

--- oopdart.dart
+++ oopdart.dart
@@ -24,6 +24,57 @@ class Student {
   }
 }
 
+class Person {
+  String? name;
+  int? age;
+  String? subject;
+  double? salary;
+
+  // รูปแบบย่อ -- ไม่ต้องเขียน this.name = name;
+  Person(this.name, this.age, this.subject, this.salary);
+
+  void display() {
+    print("ชื่อ: $name, อายุ: $age, วิชา: $subject, เงินเดือน: $salary");
+  }
+}
+
+class Point {
+  // Named Constructor
+  double x = 0;
+  double y = 0;
+
+  // คอนสตรัคเตอร์หลัก
+  Point(this.x, this.y);
+
+  // คอนสตรัคเตอร์แบบตั้งชื่อ: สร้างจุดที่จุดกำเนิด (0,0)
+  Point.origin() {
+    x = 0;
+    y = 0;
+  }
+
+  void display() => print("จุด (x: $x, y: $y)");
+}
+
+class Chair {
+  //Named Parameters
+  String? name;
+  String? color;
+
+  Chair({this.name, this.color});
+
+  void display() {
+    print("ชื่อ: $name, สี: $color");
+  }
+}
+
+class ImmutablePoint {
+  //Constant Constructor
+  final double x;
+  final double y;
+
+  const ImmutablePoint(this.x, this.y);
+}
+
 void main() {
   // Student s1 = Student();
 
@@ -39,4 +90,19 @@ void main() {
 
   Student s = Student("จอห์น", 20, 1);
   s.display();
+
+  Person p = Person("John", 30, "Mathematic", 50000);
+  p.display();
+
+  Point p1 = Point(3, 4);
+  Point p2 = Point.origin();
+  p1.display(); // จุด (x: 3.0, y: 4.0)
+  p2.display(); // จุด (x: 0.0, y: 0.0)
+  Chair chair = Chair(color: "แดง", name: "เก้าอี้ตัวที่ 1");
+  chair.display();
+  const p11 = ImmutablePoint(1, 2);
+  const p22 = ImmutablePoint(1, 2);
+
+  // p1 และ p2 ชี้ไปยังอ็อบเจกต์เดียวกันในหน่วยความจำ
+  print(identical(p11, p22)); // true
 }
