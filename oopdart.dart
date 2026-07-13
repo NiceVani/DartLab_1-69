df06a4296a057002311532e00602b3a484bc1373
NiceVani <Nice090147022165@gmail.com>
2026-07-13 21:53:52 +0700

    ll

--- oopdart.dart
+++ oopdart.dart
@@ -3,6 +3,14 @@ class Student {
   String? name;
   int? age;
   int? grade;
+  int? rollnumber;
+
+  Student(String name, int age, int rollnumber) {
+    //<-- Constructor
+    this.name = name;
+    this.age = age;
+    this.rollnumber = rollnumber;
+  }
 
   void displayInfo() {
     // <-- Medthod
@@ -10,14 +18,25 @@ class Student {
     print("อายุ: $age");
     print("ชั้นปี: $grade");
   }
+
+  void display() {
+    print("ชื่อ: $name , อายุ: $age, เลขที่: $rollnumber");
+  }
 }
 
 void main() {
-  Student s1 = Student();
+  // Student s1 = Student();
 
-  s1.name = "สมชาย";
-  s1.age = 20;
-  s1.grade = 2;
+  // s1.name = "สมชาย";
+  // s1.age = 20;
+  // s1.grade = 2;
 
-  s1.displayInfo();
+  // Student s2 = Student();
+  // s2.name = "สมหญิง";
+  // s2.age = 21;
+
+  // s1.displayInfo();
+
+  Student s = Student("จอห์น", 20, 1);
+  s.display();
 }
